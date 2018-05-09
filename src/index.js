11a6f36c3a50da5fed1f39ecdb1cf1f365b08845
opagani <opagani@zillowgroup.com>
2018-05-09 09:01:02 -0700

    adding code-splitting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import nav from "./nav";
 // import { footer } from "./footer";
+//import * as GSAP from "gsap";
+const getGSAP = () => import("gsap");
 const getFooter = () => import("./footer");
+const getLodashUniq = () => import("lodash-es/uniq");
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 import makeImage from "./image";
@@ -17,6 +20,14 @@ document.body.appendChild(button);
 button.addEventListener("click", e => {
     getFooter().then(footerModule => {
         document.body.appendChild(footerModule.footer);
+    });
+
+    getGSAP().then(gsap => {
+        console.log(gsap);
+    });
+
+    getLodashUniq(uniq => {
+        console.log(uniq);
     });
 });
 
